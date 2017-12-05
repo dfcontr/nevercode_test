252f7381aad37f08fa0ff042bc5bec85571b0d06
Daniel Contreras <daniel.contreras@outlook.com>
2017-12-04 19:26:36 -0700

    Add initial test

---
+++ App.test.js
@@ -0,0 +1,11 @@
+import 'react-native';
+import React from 'react';
+import App from './App';
+import renderer from 'react-test-renderer';
+
+describe('App component', () => {
+  it('renders correctly', () => {
+    const app = renderer.create(<App />).toJSON();
+    expect(app).toMatchSnapshot();
+  });
+});
